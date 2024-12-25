ef1511a75abeb91319a5082790d35e4e63ffdd5e
justb <justb@DESKTOP-31ALNJD>
2024-12-25 13:02:40 +0200

    last-version

---
+++ E-Commerce--Frontend/Client/src/app/shared/models/basket.ts
@@ -0,0 +1,19 @@
+export interface BasketItem{
+
+    id:number;
+    productName:string;
+    price:number;
+    quantity:number;
+    pictureUrl:string;
+    brand:string;
+    type:string;
+}
+
+export interface Basket{
+    id:string;
+    items:BasketItem[];
+}
+export class Basket implements Basket{
+    id="ahmed";
+    items: BasketItem[]=[];
+}
